b4ce5289de119328dbf920917f43bc8b864ab771
Ishanka Pramidu Lakshan <79820853+Lakshan555@users.noreply.github.com>
2021-08-04 14:52:11 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React,{useState} from 'react';
-import { Button, StyleSheet, Text, View,TextInput,ScrollView } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-
- 
   return (
- 
     <View style={styles.container}>
-
+      <View style={styles.hedar}>
+        <Text style={styles.boldText}>Hello,APP</Text>
+      </View>
+      <View style={styles.body}>
+      <Text style={styles.boldText}>Lorem ipum adwda dfscs</Text>
+      <Text>Lorem ipum adwda dfscs</Text>
+      <Text>Lorem ipum adwda dfscs</Text>
+      <Text>Lorem ipum adwda dfscs</Text>
+      </View>
     </View>
 
+    
   );
 }
 
@@ -18,8 +24,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  hedar: {
+    backgroundColor:'green',
+    padding:20,
+   
+    
+  },
+  boldText:{
+    fontWeight:'bold',
+   
+  },
+  body:{
+    backgroundColor:'yellow',
+    padding:20,
+    marginTop:10,
   }
-
-
 });
+
